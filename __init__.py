218172d47daf3e6a7c1a9853c59b6aea8f82312b
Luk Wang <1135188941@qq.com>
2022-01-31 19:45:31 +0800

    reversegif

--- __init__.py
+++ __init__.py
@@ -2,102 +2,102 @@ from botoy import logger, MsgTypes, S, GroupMsg, FriendMsg
 from botoy.sugar import Picture, Text, Voice
 from botoy.parser import friend, group
 from botoy.decorators import ignore_botself
-from botoy.session import SessionHandler, session, FILTER_SUCCESS, ctx
-import re
-from replyserver import reply_server, PicObj, REPLY_TYPE, CMD_TYPE
+from botoy.session import SessionHandler, session, FILTER_SUCCESS
+import httpx
 import time
+import os
+from PIL import Image
 
 __doc__ = """图片存储助手"""
 
 
 def session_filter(ctx):
-    if re.match('^_savepic.{1,}',ctx.Content):
+    if ctx.Content == "倒放":
         return FILTER_SUCCESS
 
-
-l_session = SessionHandler(ignore_botself, session_filter, single_user=True, expiration = 30)
-l_reply_server = reply_server()
+cur_file_dir = os.path.dirname(os.path.realpath(__file__))
+l_session = SessionHandler(ignore_botself, session_filter, single_user=True, expiration = 50)
 
 
 @l_session.parse
 def _par(ctx):
     if ctx.MsgType != MsgTypes.PicMsg:
-        return ctx.Content
+        return None
 
-    pic_obj = PicObj()
+    url = None
+
     if isinstance(ctx, FriendMsg):
         pics = friend.pic(ctx)
-        pic_obj.user = ctx.FromUin
-        pic_obj.Url = pics.FriendPic[0].Url
-        pic_obj.Md5 = pics.FriendPic[0].FileMd5
+        url = pics.FriendPic[0].Url
     elif isinstance(ctx, GroupMsg):
         pics = group.pic(ctx)
-        pic_obj.user = ctx.FromUserId
-        pic_obj.Url = pics.GroupPic[0].Url
-        pic_obj.Md5 = pics.GroupPic[0].FileMd5
+        url = pics.GroupPic[0].Url
 
-    return pic_obj
-        
+    return url
+
+
+def find_imgtype(type_str):
+    prefix = 'image/'
+    img_type = None
+    index = type_str.find(prefix)
+    if index == 0:
+        img_type = type_str[len(prefix):]
+    return img_type
+
 
 @l_session.handle
 def _h():
-    prefix = '_savepic'
-    cmd = ctx.Content[len(prefix):]
-    user_qq = ""
-    try:
-        user_qq = str(ctx.FromUin)
-    except:
-        user_qq = str(ctx.FromUserId)
+    session.send_text('指令已收到，请发送要倒放的图片')
 
-    if l_reply_server.checkout(cmd, user_qq, cmd_type=CMD_TYPE.PIC, create = True):
-        session.send_text('开启{}存储模式，请发送图片'.format(cmd))
-    else:
-        session.send_text('访问{}图片仓库失败'.format(cmd))
-        l_session.finish()
-        return
-   
     timeout = session._expiration
     last = time.monotonic()
     while True:
         if time.monotonic() - last > timeout:
-            session.send_text('因对方无响应，存储模式关闭')
+            session.send_text('因对方无响应，会话关闭')
             l_session.finish()
             break
-        item = session.pop('pic', wait = True, timeout=1)
-        if not item:
+        pic_url = session.pop('pic_url', wait = True, timeout=1)
+        if not pic_url:
             continue
-        
-        last = time.monotonic()
-        if isinstance(item, PicObj):
-            ret = l_reply_server.save_pic(item)
-            if not ret:
-                session.send_text('保存图片失败')
-            else:
-                session.send_text('保存图片成功')
-        elif item == 'end':
-            session.send_text('存储模式已关闭')
-            l_session.finish()
-            break
-        else:
-            session.send_text('无效参数')
 
+        if download_gif(pic_url):
+            reverse_gif()
+            Picture(pic_path=os.path.join(cur_file_dir, 'reversed.gif'))
+
+        else:
+            session.send_text('下载图片失败')
+
+
+def download_gif(pic_url):
+    try:
+        res = httpx.get(pic_url)
+        res.raise_for_status()
+        img_type = find_imgtype(res.headers['content-type'])
+        if not img_type:
+            raise Exception('Failed to resolve image type')
+        if img_type.upper() != "GIF":
+            return False
+        file_name = 'to_reverse.gif'
+        file_path = os.path.join(cur_file_dir, file_name)
+        logger.info('Saving image to: {}'.format(file_path))
+        with open(file_path, 'wb') as img:
+            img.write(res.content)
+        return True
+    except Exception as e:
+        logger.warning('Failed to get picture from url:{},{}'.format(pic_url, e))
+        return False
+
+    return False
+
+
+def reverse_gif():
+    im = Image.open(os.path.join(cur_file_dir, 'to_reverse.gif'))
+    frames = im.get_frames()
+    frames.reverse()
+    frames[0].save(os.path.join(cur_file_dir, 'reversed.gif'), save_all=True, append_images=frames[1:])
 
 @ignore_botself
 def main(ctx):
     l_session.message_receiver(ctx)
-    if not l_session.sc.session_existed(ctx, True):
-        l_reply_server.handle_cmd(ctx)
-        reply_type = l_reply_server.reply_type
-        if reply_type == REPLY_TYPE.PIC_MD5:
-            replier = S.bind(ctx)
-            replier.image(data=l_reply_server.reply, type=S.TYPE_MD5)
-        elif reply_type == REPLY_TYPE.PIC_PATH:
-            Picture(pic_path=l_reply_server.reply)
-        elif reply_type == REPLY_TYPE.TEXT:
-            Text(l_reply_server.reply)
-        elif reply_type == REPLY_TYPE.VOICE:
-            logger.info("send voice: "+l_reply_server.reply)
-            Voice(voice_path=l_reply_server.reply)
-
 
 receive_group_msg=receive_friend_msg=main
