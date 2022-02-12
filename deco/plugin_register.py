91c3e9333a6fabbeec3c2f679aaf26db4dcd0341
Luk Wang <1135188941@qq.com>
2022-02-12 15:35:29 +0800

    help content

--- deco/plugin_register.py
+++ deco/plugin_register.py
@@ -3,7 +3,7 @@ from ..cmd_server import plugin_manager
 import re
 
 
-def plugin_register(name: str, help=""):
+def plugin_register(name: str, help_content=""):
     def deco(func):
         def inner(ctx: GroupMsg):
             ret = None
@@ -11,7 +11,10 @@ def plugin_register(name: str, help=""):
             if h_plugin.bind(ctx):
                 if re.match(f"^帮助\s*{name}$", ctx.Content):
                     sender = S.bind(ctx)
-                    sender.text(help)
+                    if len(help_content):
+                        sender.text(help_content)
+                    else:
+                        sender.text("没有找到帮助说明\u1F97A")
                 else:
                     ret = func(ctx)
 
